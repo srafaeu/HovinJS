8a545e063491c819e05094acc91c936ab10cb108
srafaeu <srafaeu@gmail.com>
2015-05-25 18:18:05 -0300

    Documentation
    Upgrade documentation

--- src/scripts/hovinjs/Basics/Angle.js
+++ src/scripts/hovinjs/Basics/Angle.js
@@ -5,8 +5,8 @@ var Angle = function() {};
 
 /**
  * Convert radians to degrees
- * @param {Number} radians Value of an angle in radians
- * @returns {Number} Value of an angle in degrees between 0 and 360
+ * @param {number} radians Value of an angle in radians
+ * @returns {number} Value of an angle in degrees between 0 and 360
  */
 Angle.toDegrees = function(radians) {
 	return (radians * (180 / Math.PI));
@@ -14,8 +14,8 @@ Angle.toDegrees = function(radians) {
 
 /**
  * Convert degrees to radians
- * @param {Number} degrees Value of an angle in degrees
- * @returns {Number} Value of an angle in radians between 0 and 2&pi;
+ * @param {number} degrees Value of an angle in degrees
+ * @returns {number} Value of an angle in radians between 0 and 2&pi;
  */
 Angle.toRadians = function(degrees) {
 	return (degrees * (Math.PI / 180));
--- src/scripts/hovinjs/Basics/Random.js
+++ src/scripts/hovinjs/Basics/Random.js
@@ -1,23 +1,41 @@
-/* class Random */
+/**
+ * Create a Random object to generate values
+ * @class Random
+ */
 var Random = function() {};
 
+/**
+ * Returns a random number between 0 (inclusive) and 1 (exclusive)
+ * @method next
+ * @return {number} Floating-point, pseudo-random number in the range 0 (inclusive) and 1 (exclusive)
+ */
+Random.next = 
 Random.prototype.next = function() {
 	return Math.random();
 };
 
-Random.prototype.nextInt = function(max, zero) {
-	return Math.floor(nextFloat(max, zero));
+/**
+ * Returns a random number between minimum and maximum value
+ * @method nextInt
+ * @param {number} min Integer number for minimum value
+ * @param {number} max Integer number for maximum value
+ * @return {number} Integer, pseudo-random number in the Minimum value (inclusive) and Maximum value (exclusive)
+ */
+Random.nextInt = 
+Random.prototype.nextInt = function(min, max) {
+	return Math.floor(this.nextFloat(min, max));
 };
 
-Random.prototype.nextFloat = function(max, zero) {
-	var r = 0;
-	var m = max || 1;
-	var z = zero || false;
+/**
+ * Returns a random number between minimum and maximum value
+ * @method nextFloat
+ * @param {number} min Floating-point number for minimum value
+ * @param {number} max Floating-point number for maximum value
+ * @return {number} Floating-point, pseudo-random number in the Minimum value (inclusive) and Maximum value (exclusive)
+ */
+Random.nextFloat = 
+Random.prototype.nextFloat = function(min, max) {
+	var a = max || 1, b = min || 0;
 	
-	if (z)
-		r = Math.random() * (m + 1);
-	else
-		r = (Math.random() * (m)) + 1;
-	
-	return r;
+	return Math.random() * (max - min) + min;
 };
